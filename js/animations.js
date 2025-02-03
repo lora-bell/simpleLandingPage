28b4e48577266dfa79bd8b54f88b275069a1b52f
Lora Bell <plutovka-z@rambler.ru>
2025-02-03 21:13:30 +0300

    change blocks

--- js/animations.js
+++ js/animations.js
@@ -1,10 +1,14 @@
-const ANIMATION_SPEED = 50
+const ANIMATION_SPEED = 90
 
+function generateFromTo(min, max){
+    return Number((Math.random() * (max - min) + min).toFixed())
+}
+
+// анимация счетчика количества клиентов
 function increaseAnimationStep(count, element, endNumber){
-    console.log("element.innerText " + element.innerText)
     if(count <= endNumber){
         element.innerText = count
-        count += 255
+        count += generateFromTo(150, 250)
         setTimeout(increaseAnimationStep, ANIMATION_SPEED, count, element, endNumber)
     }else{
         element.innerText = "5000+"
@@ -13,3 +17,26 @@ function increaseAnimationStep(count, element, endNumber){
 
 element = document.querySelector(".features__clients-count")
 increaseAnimationStep(100, element, 5000)
+
+// выбор стоимости, поле "Другое"
+document.querySelector("#price").addEventListener("change",
+    function handleSelectChange(event){
+        if(event.target.value === "other"){
+            let input = document.createElement("input")
+            input.placeholder = "Введите ваш вариант (числовое значение)"
+            input.type = "number"
+            input.min = 0
+
+            let formOtherInput = document.createElement("div")
+            formOtherInput.classList.add("form__group", "form__other-input")
+            formOtherInput.append(input)
+
+            let formContainerForm = document.querySelector(".form_container form")
+            formContainerForm.append(formOtherInput)
+        } else {
+            let formOtherInput = document.querySelector(".form__other-input")
+            if(formOtherInput){
+                formOtherInput.remove()
+            }
+        }
+})
